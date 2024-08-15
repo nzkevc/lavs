aa1a1e19b8b993033424fd9db836d157c4fa8adb
Gluglug7 <abod278@aucklanduni.ac.nz>
2024-08-16 00:48:21 +1200

    refactor: making customerRepository create and update the same format as other repository classes

--- src/main/java/uoa/lavs/repository/CustomerRepository.java
+++ src/main/java/uoa/lavs/repository/CustomerRepository.java
@@ -8,22 +8,39 @@ import uoa.lavs.models.Customer;
 import uoa.lavs.utils.ConnectionInstance;
 
 public class CustomerRepository {
-  public static Customer create(Customer customer) {
-    customer.setId(null);
-    return update(customer);
-  }
-
-  public static Customer update(Customer customer) throws RuntimeException {
-    Connection connection = ConnectionInstance.getConnection();
-
-    UpdateCustomer message = new UpdateCustomer();
-
+  private static UpdateCustomer persist(UpdateCustomer message, Customer customer) {
     message.setCustomerId(customer.getId());
     message.setName(customer.getName());
     message.setTitle(customer.getTitle());
     message.setDateofBirth(customer.getDateOfBirth());
     message.setOccupation(customer.getOccupation());
     message.setCitizenship(customer.getCitizenship());
+
+    return message;
+  }
+
+  public static Customer create(Customer customer) {
+    Connection connection = ConnectionInstance.getConnection();
+
+    UpdateCustomer message = new UpdateCustomer();
+    customer.setId(null);
+    message = persist(message, customer);
+
+    Status status = message.send(connection);
+
+    if (!status.getWasSuccessful()) {
+      throw new RuntimeException("Failed to create customer: " + status.getErrorMessage());
+    } else {
+      customer.setId(message.getCustomerIdFromServer());
+      return customer;
+    }
+  }
+
+  public static Customer update(Customer customer) throws RuntimeException {
+    Connection connection = ConnectionInstance.getConnection();
+
+    UpdateCustomer message = new UpdateCustomer();
+    message = persist(message, customer);
 
     Status status = message.send(connection);
 
