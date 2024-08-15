286c8e317d03c0981ac277313fe5701a8518505a
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-08-15 14:45:35 +1200

    chore: unformat Craig mainframe file code

--- src/main/java/uoa/lavs/mainframe/messages/customer/UpdateCustomerNote.java
+++ src/main/java/uoa/lavs/mainframe/messages/customer/UpdateCustomerNote.java
@@ -2,140 +2,96 @@ package uoa.lavs.mainframe.messages.customer;
 
 import uoa.lavs.mainframe.*;
 
-public class UpdateCustomerNote implements Message, MessageDescription, UpdateCustomerChildMessage {
-  public static final int REQUEST_CODE = 1206;
-  private Response response;
-  private String customerId;
-  private Integer number;
-  private String[] line = new String[20];
+public class UpdateCustomerNote implements Message, MessageDescription {
+    public static final int REQUEST_CODE = 1206;
+    private Response response;
+    private String customerId;
+    private Integer number;
+    private String[] line= new String[20];
 
-  public class Fields {
-    public static final String[] INPUT = {
-      "[01].line",
-      "[02].line",
-      "[03].line",
-      "[04].line",
-      "[05].line",
-      "[06].line",
-      "[07].line",
-      "[08].line",
-      "[09].line",
-      "[10].line",
-      "[11].line",
-      "[12].line",
-      "[13].line",
-      "[14].line",
-      "[15].line",
-      "[16].line",
-      "[17].line",
-      "[18].line",
-      "[19].line",
-      "[20].line",
-      "id",
-      "number"
-    };
-    public static final String[] OUTPUT = {
-      "[01].line",
-      "[02].line",
-      "[03].line",
-      "[04].line",
-      "[05].line",
-      "[06].line",
-      "[07].line",
-      "[08].line",
-      "[09].line",
-      "[10].line",
-      "[11].line",
-      "[12].line",
-      "[13].line",
-      "[14].line",
-      "[15].line",
-      "[16].line",
-      "[17].line",
-      "[18].line",
-      "[19].line",
-      "[20].line",
-      "lines",
-      "number",
-      "pages"
-    };
+    public class Fields {
+        public static final String[] INPUT = {"[01].line", "[02].line", "[03].line", "[04].line", "[05].line", "[06].line", "[07].line", "[08].line", "[09].line", "[10].line", "[11].line", "[12].line", "[13].line", "[14].line", "[15].line", "[16].line", "[17].line", "[18].line", "[19].line", "[20].line", "id", "number"};
+        public static final String[] OUTPUT = {"[01].line", "[02].line", "[03].line", "[04].line", "[05].line", "[06].line", "[07].line", "[08].line", "[09].line", "[10].line", "[11].line", "[12].line", "[13].line", "[14].line", "[15].line", "[16].line", "[17].line", "[18].line", "[19].line", "[20].line", "lines", "pages"};
 
-    public static final String CUSTOMER_ID = "id";
-    public static final String NUMBER = "number";
-    public static final String PAGE_COUNT = "pages";
-    public static final String LINE_COUNT = "lines";
-    public static final String LINE = "[%02d].line";
-  }
-
-  @Override
-  public Status send(Connection connection) {
-    Request request = new Request(REQUEST_CODE);
-    if (customerId != null) request.setValue(Fields.CUSTOMER_ID, customerId);
-    if (number != null) request.setValue(Fields.NUMBER, number.toString());
-    for (int loop = 0; loop < 20; loop++) {
-      if (line[loop] != null) {
-        request.setValue(String.format(Fields.LINE, loop), line[loop].toString());
-      }
+        public static final String CUSTOMER_ID = "id";
+        public static final String NUMBER = "number";
+        public static final String PAGE_COUNT = "pages";
+        public static final String LINE_COUNT = "lines";
+        public static final String LINE = "[%02d].line";
     }
-    response = connection.send(request);
-    return response.getStatus();
-  }
 
-  // sets customer id [customerId]
-  public void setCustomerId(String value) throws IllegalArgumentException {
-    if (value != null && value.length() > 10) {
-      throw new IllegalArgumentException("customerId is too long - max length is 10");
+    @Override
+    public Status send(Connection connection) {
+        Request request = new Request(REQUEST_CODE);
+        if (customerId != null) request.setValue(Fields.CUSTOMER_ID, customerId.toString());
+        if (number != null) request.setValue(Fields.NUMBER, number.toString());
+        for (int loop = 0; loop < 20; loop++) {
+            if (line[loop] != null) {
+                request.setValue(
+                    String.format(Fields.LINE, loop),
+                    line[loop].toString());
+            }
+        }
+        response = connection.send(request);
+        return response.getStatus();
     }
-    customerId = value;
-  }
 
-  // sets number [number]
-  public void setNumber(Integer value) {
-    number = value;
-  }
+    // sets customer id [customerId]
+    public void setCustomerId(String value)
+        throws IllegalArgumentException
+     {
+        if (value != null && value.length() > 10) {
+            throw new IllegalArgumentException("customerId is too long - max length is 10");
+        }
+        customerId = value;
+     }
 
-  // sets line [line]
-  public void setLine(Integer row, String value) {
-    line[row] = value;
-  }
+    // sets number [number]
+    public void setNumber(Integer value)
+     {
+        number = value;
+     }
 
-  // gets number from server
-  public Integer getNumberFromServer() {
-    String key = Fields.NUMBER;
-    String value = response.getValue(key);
-    if (value == null) return null;
-    return Integer.parseInt(value);
-  }
+    // sets line [line]
+    public void setLine(Integer row, String value)
+     {
+        line[row] = value;
+     }
 
-  // gets page count [pages] from server
-  public Integer getPageCountFromServer() {
-    String key = Fields.PAGE_COUNT;
-    String value = response.getValue(key);
-    if (value == null) return null;
-    return Integer.parseInt(value);
-  }
+    // gets page count [pages] from server
+    public Integer getPageCountFromServer()
+     {
+        String key = Fields.PAGE_COUNT;
+        String value = response.getValue(key);
+        if (value == null) return null;
+        return Integer.parseInt(value);
+     }
 
-  // gets line count [lines] from server
-  public Integer getLineCountFromServer() {
-    String key = Fields.LINE_COUNT;
-    String value = response.getValue(key);
-    if (value == null) return null;
-    return Integer.parseInt(value);
-  }
+    // gets line count [lines] from server
+    public Integer getLineCountFromServer()
+     {
+        String key = Fields.LINE_COUNT;
+        String value = response.getValue(key);
+        if (value == null) return null;
+        return Integer.parseInt(value);
+     }
 
-  // gets line from server
-  public String getLineFromServer(Integer row) {
-    String key = String.format(Fields.LINE, row);
-    return response.getValue(key);
-  }
+    // gets line from server
+    public String getLineFromServer(Integer row)
+     {
+        String key = String.format(Fields.LINE, row);
+        return response.getValue(key);
+     }
 
-  @Override
-  public String[] getInputFields() {
-    return Fields.INPUT;
-  }
+    @Override
+    public String[] getInputFields()
+    {
+        return Fields.INPUT;
+    }
 
-  @Override
-  public String[] getOutputFields() {
-    return Fields.OUTPUT;
-  }
+    @Override
+    public String[] getOutputFields()
+    {
+        return Fields.OUTPUT;
+    }
 }
