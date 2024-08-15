8e40e8d248c7603c72a0875bfff3e2ed9c4b3a02
Oculux <nate.williamson.nz@gmail.com>
2024-08-15 18:54:29 +1200

    feat: enable dynamic screen sizing

--- src/main/java/uoa/lavs/App.java
+++ src/main/java/uoa/lavs/App.java
@@ -1,12 +1,15 @@
 package uoa.lavs;
 
 import java.io.IOException;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import uoa.lavs.controllers.MainController;
 import uoa.lavs.utils.AsyncUtils;
 import uoa.lavs.utils.ResourceUtils;
@@ -18,6 +21,7 @@ import uoa.lavs.utils.ResourceUtils;
 public class App extends Application {
 
   private static final Logger logger = LoggerFactory.getLogger(App.class);
+  private static Scene scene;
 
   public static void main(String[] args) {
     launch();
@@ -28,8 +32,9 @@ public class App extends Application {
     logger.info("Starting application");
 
     // Load scene
-    Scene scene = new Scene(MainController.getRoot(), 640, 480);
+    scene = new Scene(new AnchorPane(), 640, 480); // Temporary values
     stage.setScene(scene);
+    scene.setRoot(MainController.getRoot());
 
     // Load resources
     ResourceUtils.loadFont("Montserrat-Medium.ttf");
@@ -49,5 +54,9 @@ public class App extends Application {
   public void stop() {
     logger.info("Stopping application");
     AsyncUtils.close();
+  }
+
+  public static Scene getScene() {
+    return scene;
   }
 }
--- src/main/java/uoa/lavs/controllers/MainController.java
+++ src/main/java/uoa/lavs/controllers/MainController.java
@@ -5,9 +5,11 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uoa.lavs.App;
 import uoa.lavs.State;
 import uoa.lavs.utils.ControllerUtils;
 import uoa.lavs.utils.ResourceUtils;
@@ -17,20 +19,65 @@ public class MainController implements IController, Initializable {
   Logger logger = LoggerFactory.getLogger(MainController.class);
 
   private static final Parent root = ResourceUtils.loadFxml("main.fxml");
+  private static final int DEFAULT_WIDTH = 1920;
+  private static final int DEFAULT_HEIGHT = 1080;
+
+  private double zoom = 1;
 
   public static Parent getRoot() {
     return root;
   }
 
-  @FXML private Pane panPage;
+  @FXML private Pane panResponsive; // Responsible for scaling the content
+  @FXML private Pane panPage; // Responsible for swapping pages
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    // Bind page
+
+    // Page state listener
     State.page.addListener(
         (observable, oldPage, newPage) -> {
           logger.debug("Page changed to: " + newPage);
           ControllerUtils.swapComponent(panPage, newPage);
         });
+
+    // Screen resize listeners
+    Scene scene = App.getScene();
+    scene
+        .heightProperty()
+        .addListener(
+            (obs, oldVal, newVal) -> {
+              updateOuterPaneSize(scene.getWidth(), newVal.doubleValue());
+            });
+    scene
+        .widthProperty()
+        .addListener(
+            (obs, oldVal, newVal) -> {
+              updateOuterPaneSize(newVal.doubleValue(), scene.getHeight());
+            });
+  }
+
+  /**
+   * Updates the screen dimensions to fill the scene. Will scale inner contents. Will maintain
+   * original aspect ratio, adding borders if neccessary (screen centered).
+   *
+   * @param sceneWidth The current width of the scene.
+   * @param sceneHeight The current height of the scene.
+   */
+  private void updateOuterPaneSize(double sceneWidth, double sceneHeight) {
+    // Height to set outer pane to be (maintain aspect ratio)
+    double aspectRatio = (double) DEFAULT_WIDTH / DEFAULT_HEIGHT;
+    Double screenWidth = Math.min(sceneWidth, sceneHeight * aspectRatio);
+
+    // Set outer pane size
+    zoom = screenWidth / DEFAULT_WIDTH;
+    panResponsive.setScaleX(zoom);
+    panResponsive.setScaleY(zoom);
+
+    // Center content
+    double horizontalShift = (sceneWidth - DEFAULT_WIDTH) / 2;
+    double verticalShift = (sceneHeight - DEFAULT_HEIGHT) / 2;
+    panResponsive.setLayoutX(horizontalShift);
+    panResponsive.setLayoutY(verticalShift);
   }
 }
