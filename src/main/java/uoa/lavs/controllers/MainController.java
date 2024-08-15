7f0ad8cf5754b9fa88e43010fa91f7229ad1c444
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-08-15 20:10:19 +1200

    refactor: remove implements Initializable, put FXML annotation on initalize instead

--- src/main/java/uoa/lavs/controllers/MainController.java
+++ src/main/java/uoa/lavs/controllers/MainController.java
@@ -1,9 +1,6 @@
 package uoa.lavs.controllers;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.layout.Pane;
 import org.slf4j.Logger;
@@ -12,7 +9,7 @@ import uoa.lavs.State;
 import uoa.lavs.utils.ControllerUtils;
 import uoa.lavs.utils.ResourceUtils;
 
-public class MainController implements IController, Initializable {
+public class MainController implements IController {
 
   Logger logger = LoggerFactory.getLogger(MainController.class);
 
@@ -24,8 +21,8 @@ public class MainController implements IController, Initializable {
 
   @FXML private Pane panPage;
 
-  @Override
-  public void initialize(URL location, ResourceBundle resources) {
+  @FXML
+  public void initialize() {
     // Bind page
     State.page.addListener(
         (observable, oldPage, newPage) -> {
