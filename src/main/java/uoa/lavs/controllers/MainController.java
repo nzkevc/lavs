a2b0a07ca70b08e878c90de6e92605b3a81d20ab
Oculux <nate.williamson.nz@gmail.com>
2024-08-15 12:55:25 +1200

    feat: create proof of concept that pages can be swapped in main controller

--- src/main/java/uoa/lavs/controllers/MainController.java
+++ src/main/java/uoa/lavs/controllers/MainController.java
@@ -1,5 +1,29 @@
 package uoa.lavs.controllers;
 
-public class MainController implements IController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+import uoa.lavs.utils.ControllerUtils;
+
+public class MainController implements IController, Initializable {
+
+  Logger logger = LoggerFactory.getLogger(MainController.class);
+
+  @FXML private Pane panPage;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    logger.info("Test");
+    logger.debug(panPage.toString());
+
+    Node temp = panPage.getChildren().get(0);
+    ControllerUtils.swapComponent(panPage, temp);
+  }
 }
