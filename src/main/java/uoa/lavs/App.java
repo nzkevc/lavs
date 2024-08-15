529beaaa3c45a7a689e6b897b51b25a71d78ad75
Oculux <nate.williamson.nz@gmail.com>
2024-08-16 09:18:47 +1200

    chore: alter exit fullscreen hint to say F11 not ESC

--- src/main/java/uoa/lavs/App.java
+++ src/main/java/uoa/lavs/App.java
@@ -2,16 +2,19 @@ package uoa.lavs;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uoa.lavs.controllers.MainController;
+import uoa.lavs.mainframe.simulator.NitriteConnection;
 import uoa.lavs.utils.AsyncUtils;
+import uoa.lavs.utils.ConnectionInstance;
 import uoa.lavs.utils.ResourceUtils;
 
 /**
@@ -38,13 +41,6 @@ public class App extends Application {
     stage.setScene(scene);
     scene.setRoot(MainController.getRoot());
 
-    // Set up fullscreen
-    scene.setOnKeyPressed(
-        event -> {
-          if (event.getCode() == javafx.scene.input.KeyCode.F11)
-            stage.setFullScreen(!stage.isFullScreen());
-        });
-
     // Load resources
     ResourceUtils.loadFont("Montserrat-Medium.ttf");
     scene.getStylesheets().add(ResourceUtils.loadCss("globals.css"));
@@ -53,9 +49,20 @@ public class App extends Application {
     scene.setFill(Color.web("#131d23"));
     stage.setTitle("Countrywide Bank: Loan System");
     stage.getIcons().add(ResourceUtils.loadImage("countrywide-bank-logo.png"));
-    stage.setFullScreen(true);
 
+    // Set up fullscreen
+    stage.setMaximized(true);
+    stage.setFullScreen(true);
+    stage.setFullScreenExitHint("Press F11 to exit full screen mode.");
+    scene.setOnKeyPressed(
+        event -> {
+          if (event.getCode() == KeyCode.F11) stage.setFullScreen(!stage.isFullScreen());
+        });
+
+    // Set up application
+    ConnectionInstance.setConnection(new NitriteConnection("testing/nitrite.db"));
     State.reset();
+
     stage.show();
   }
 
