ef88d92642e07edd1929183d0370e26fe4c0fee0
Oculux <nate.williamson.nz@gmail.com>
2024-08-18 16:09:54 +1200

    feat: create example component

--- src/main/java/uoa/lavs/controllers/pages/ExampleController.java
+++ src/main/java/uoa/lavs/controllers/pages/ExampleController.java
@@ -2,14 +2,14 @@ package uoa.lavs.controllers.pages;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uoa.lavs.State;
 import uoa.lavs.controllers.IController;
+import uoa.lavs.controllers.fragments.FieldController;
 import uoa.lavs.services.ExampleService;
 import uoa.lavs.utils.AsyncUtils;
 import uoa.lavs.utils.ResourceUtils;
@@ -24,6 +24,8 @@ public class ExampleController implements IController {
 
   @FXML private Label lblTitle;
 
+  @FXML private FieldController fieldController;
+
   public static Component<ExampleController> getSingleton() {
     return singleton;
   }
@@ -36,9 +38,16 @@ public class ExampleController implements IController {
   @FXML
   private void createUser() throws IOException, InterruptedException, ExecutionException {
 
-    String name = "John Doe"; // Imagine this is read from a text field
+    String name = fieldController.getValue();
 
-    AsyncUtils.promise(() -> ExampleService.createUser(name))
-        .addListener(() -> logger.info("User created successfully"), Runnable::run);
+    AsyncUtils.promise(
+        () -> ExampleService.createUser(name),
+        user -> {
+          logger.info("User created: " + user);
+        },
+        throwable -> {
+          logger.warn("Error creating user", throwable);
+          Platform.runLater(() -> fieldController.setError("Error creating user"));
+        });
   }
 }
