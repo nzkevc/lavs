8a247abd7fb5d419e668fcf8f63a66a3307d2e09
Oculux <nate.williamson.nz@gmail.com>
2024-08-20 00:35:28 +1200

    feat: implement navigation between pages
    Using landing page for now just as a placeholder.

--- src/main/java/uoa/lavs/controllers/pages/SummaryPageController.java
+++ src/main/java/uoa/lavs/controllers/pages/SummaryPageController.java
@@ -5,29 +5,34 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import uoa.lavs.controllers.IController;
+import uoa.lavs.controllers.MainController;
 import uoa.lavs.utils.ControllerUtils;
 
 public class SummaryPageController extends AnchorPane implements IController {
 
-    @FXML private Button backBtn;
+  @FXML private Button backBtn;
 
-    @FXML private Label errorLbl;
-    @FXML private Button submitBtn;
+  @FXML private Label errorLbl;
+  @FXML private Button submitBtn;
 
-    @FXML private AnchorPane infoPane;
-    @FXML private Label customerName;
-    @FXML private Label customerID;
+  @FXML private AnchorPane infoPane;
+  @FXML private Label customerName;
+  @FXML private Label customerID;
 
-    @FXML private Button generalBtn;
-    @FXML private Button contactBtn;
-    @FXML private Button employerBtn;
-    @FXML private Button notesBtn;
-    @FXML private Button loansBtn;
+  @FXML private Button generalBtn;
+  @FXML private Button contactBtn;
+  @FXML private Button employerBtn;
+  @FXML private Button notesBtn;
+  @FXML private Button loansBtn;
 
-    @FXML private AnchorPane infoCard;
-    
+  @FXML private AnchorPane infoCard;
+
   public SummaryPageController() {
-     ControllerUtils.loadFxml(this, "pages/summary-page.fxml");
+    ControllerUtils.loadFxml(this, "pages/summary-page.fxml");
   }
-    
+
+  @FXML
+  private void onBackBtnClick() {
+    MainController.getInstance().switchPage(LandingPageController.class);
+  }
 }
