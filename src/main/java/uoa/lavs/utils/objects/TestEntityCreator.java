0ae8cfe6cc797111bdd291915cff023d43846e57
Oculux <nate.williamson.nz@gmail.com>
2024-08-20 02:26:55 +1200

    chore: copy over Adam's TestEntityCreator (temporary)

---
+++ src/main/java/uoa/lavs/utils/objects/TestEntityCreator.java
@@ -0,0 +1,165 @@
+package uoa.lavs.utils.objects;
+
+import java.time.LocalDate;
+
+import uoa.lavs.mainframe.Frequency;
+import uoa.lavs.mainframe.RateType;
+import uoa.lavs.models.Address;
+import uoa.lavs.models.Customer;
+import uoa.lavs.models.Email;
+import uoa.lavs.models.Employer;
+import uoa.lavs.models.Loan;
+import uoa.lavs.models.Phone;
+
+// Nate: copy of Adam's TestEntityCreator - temporary for the demo
+public class TestEntityCreator {
+
+  // This method is new - assumes one address, phone, email, employer, and loan
+  public static Customer createFullCustomer() {
+    Customer customer = createBasicCustomer();
+    customer.getAddresses().setResidentialAddress(createBasicPrimaryAddress(customer));
+    customer.getPhones().setPrimaryPhone(createBasicPrimaryPhone(customer));
+    customer.getEmails().setPrimaryEmail(createBasicEmail(customer));
+    customer.setEmployer(createBasicEmployer(customer));
+    customer.getLoans().addLoan(createBasicLoan(customer));
+    return customer;
+  }
+
+  public static Customer createBasicCustomer() {
+    LocalDate dateOfBirth = LocalDate.of(1990, 1, 1);
+    return new Customer.Builder(
+            "1", "Mr", "John Doe", dateOfBirth, "Consultant", "New Zealand", "N/A")
+        .build();
+  }
+
+  public static Address createBasicAddress(Customer customer) {
+    return new Address(
+        customer.getId(),
+        null,
+        "Home",
+        "123 Fake St",
+        null,
+        "Fakeville",
+        "Faketown",
+        "1234",
+        "New Zealand",
+        true,
+        true);
+  }
+
+  public static Address createBasicPrimaryAddress(Customer customer) {
+    return new Address(
+        customer.getId(),
+        null,
+        "Home",
+        "123 Fake St",
+        null,
+        "Fakeville",
+        "Faketown",
+        "1234",
+        "New Zealand",
+        true,
+        false);
+  }
+
+  public static Address createBasicMailingAddress(Customer customer) {
+    return new Address(
+        customer.getId(),
+        null,
+        "Home",
+        "123 Faker Ln",
+        null,
+        "Fakerton",
+        "Fakecity",
+        "1234",
+        "New Zealand",
+        false,
+        true);
+  }
+
+  public static Address createBasicSecondaryAddress(Customer customer) {
+    return new Address(
+        customer.getId(),
+        null,
+        "Home",
+        "321 Fakes Rd",
+        null,
+        "Fakecity",
+        "Fakecity",
+        "2312",
+        "New Zealand",
+        false,
+        false);
+  }
+
+  public static Email createBasicEmail(Customer customer) {
+    return new Email(customer.getId(), null, "fakeEmail@fake.com", true);
+  }
+
+  public static Email createBasicNonPrimaryEmail(Customer customer) {
+    return new Email(customer.getId(), null, "fakeEmail2@fake.com", false);
+  }
+
+  public static Phone createBasicPhone(Customer customer) {
+    return new Phone(customer.getId(), null, "Mobile", "021", "1234567", true, true);
+  }
+
+  public static Phone createBasicPrimaryPhone(Customer customer) {
+    return new Phone(customer.getId(), null, "Home", "09", "1234567", true, false);
+  }
+
+  public static Phone createBasicTextPhone(Customer customer) {
+    return new Phone(customer.getId(), null, "Mobile", "022", "7654321", false, true);
+  }
+
+  public static Phone createBasicSecondaryPhone(Customer customer) {
+    return new Phone(customer.getId(), null, "Work", "09", "123987", false, false);
+  }
+
+  public static Employer createBasicEmployer(Customer customer) {
+    return new Employer(
+        customer.getId(),
+        "Fake Employer",
+        createBasicAddress(customer),
+        createBasicPhone(customer),
+        createBasicEmail(customer),
+        "www.fakeemployer.com",
+        true);
+  }
+
+  public static Loan createBasicLoan(Customer customer) {
+    LocalDate startDate = LocalDate.of(1990, 1, 1);
+    return new Loan(
+        customer.getId(),
+        null,
+        customer.getName(),
+        1000.00,
+        startDate,
+        10,
+        5,
+        0.05,
+        RateType.Floating,
+        Frequency.Weekly,
+        100.00,
+        Frequency.Monthly,
+        true);
+  }
+
+  public static Loan createBasicSecondaryLoan(Customer customer) {
+    LocalDate startDate = LocalDate.of(1991, 2, 2);
+    return new Loan(
+        customer.getId(),
+        null,
+        customer.getName(),
+        2000.00,
+        startDate,
+        20,
+        10,
+        0.10,
+        RateType.Fixed,
+        Frequency.Monthly,
+        200.00,
+        Frequency.Fortnightly,
+        false);
+  }
+}
