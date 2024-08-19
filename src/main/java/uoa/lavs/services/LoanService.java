6565ae42e1e199fec1a9df00a16cb7b0e15a597e
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-08-19 18:44:53 +1200

    feat: implement attaching loan summary and payments to loan when getting

--- src/main/java/uoa/lavs/services/LoanService.java
+++ src/main/java/uoa/lavs/services/LoanService.java
@@ -1,7 +1,9 @@
 package uoa.lavs.services;
 
 import uoa.lavs.models.*;
+import uoa.lavs.repository.LoanPaymentsRepository;
 import uoa.lavs.repository.LoanRepository;
+import uoa.lavs.repository.LoanSummaryRepository;
 import uoa.lavs.repository.LoansRepository;
 
 public class LoanService implements IService {
@@ -9,15 +11,18 @@ public class LoanService implements IService {
   public static Loans getLoans(Customer customer) throws RuntimeException {
     try {
       Loans loans = LoansRepository.get(customer);
+      for (Loan loan : loans.getLoans()) {
+        LoanSummary loanSummary = LoanSummaryRepository.get(new LoanSummary(loan.getLoanId()));
+        loan.setLoanSummary(loanSummary);
+
+        LoanPayments loanPayments =
+            LoanPaymentsRepository.get(new LoanPayments(loan.getLoanId(), 1));
+        loan.setLoanPayments(loanPayments);
+      }
       return loans;
     } catch (Exception e) {
       return new Loans(customer.getId());
     }
-  }
-
-  public static Loan getLoanById(String loanId) throws RuntimeException {
-    Loan loan = LoanRepository.get(loanId);
-    return loan;
   }
 
   public static void createLoansFromCustomer(Customer customer) throws RuntimeException {
