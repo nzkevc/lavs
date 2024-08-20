55361db3b81882801cd23f8a701c9f3d07907c47
Oculux <nate.williamson.nz@gmail.com>
2024-08-21 04:10:33 +1200

    bug: clear fields when click back

--- src/main/java/uoa/lavs/controllers/pages/SummaryPageController.java
+++ src/main/java/uoa/lavs/controllers/pages/SummaryPageController.java
@@ -103,6 +103,7 @@ public class SummaryPageController extends AnchorPane implements IPage {
 
   @FXML
   private void onBackBtnClick() {
+    clearAll();
     App.getMainController().switchPage(SearchPageController.class);
   }
 
