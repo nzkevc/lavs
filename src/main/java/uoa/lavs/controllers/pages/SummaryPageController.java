239eb9aa488d4d45076afc9cdba881484f04198b
Oculux <nate.williamson.nz@gmail.com>
2024-08-20 21:21:21 +1200

    bug: render customer before displaying status message

--- src/main/java/uoa/lavs/controllers/pages/SummaryPageController.java
+++ src/main/java/uoa/lavs/controllers/pages/SummaryPageController.java
@@ -2,15 +2,13 @@ package uoa.lavs.controllers.pages;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uoa.lavs.App;
 import uoa.lavs.State;
 import uoa.lavs.controllers.cards.ContactCardController;
@@ -196,13 +194,12 @@ public class SummaryPageController extends AnchorPane implements IPage {
           return customer;
         },
         (customer) -> {
-          State.clearMessage();
+          renderCustomer(customer);
           if (isCreating) {
             State.setMessageSuccess("Customer created successfully with id: " + customer.getId());
           } else {
             State.setMessageSuccess("Customer updated successfully with id: " + customer.getId());
           }
-          renderCustomer(customer);
         },
         this::handleException);
   }
