6fe21d5431c6a391450a2a583fd76c7a166decb6
hayley9080 <hsmi309@aucklanduni.ac.nz>
2024-08-20 21:47:34 +1200

    feat: added fields to controller for loan

--- src/main/java/uoa/lavs/controllers/fragments/LoanBoxController.java
+++ src/main/java/uoa/lavs/controllers/fragments/LoanBoxController.java
@@ -1,10 +1,14 @@
 package uoa.lavs.controllers.fragments;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Accordion;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import uoa.lavs.controllers.cards.ICard;
+import uoa.lavs.mainframe.Frequency;
 import uoa.lavs.models.Loan;
 import uoa.lavs.utils.ControllerUtils;
 
@@ -13,21 +17,39 @@ public class LoanBoxController extends VBox implements ICard<Loan> {
 
     @FXML private Accordion accordion;
 
-    // @FXML private FieldController status;
-    // @FXML private FieldController principleCents;
-    // @FXML private FieldController startDate;
-    // @FXML private FieldController periodMonths;
-    // //@FXML private FieldController term;
-    // @FXML private FieldController interestRate;
-    // @FXML private FieldController rateType;
-    // @FXML private FieldController compoundingFrequency;
-    // @FXML private FieldController paymentAmountCents;
-    // //@FXML private FieldController paymentFrequency;
-    // @FXML private FieldController interestOnly;
-    // @FXML private FieldController loanPayments;
-    // @FXML private FieldController loanSummary;
+    //loan details
+    @FXML private FieldController status;
+    @FXML private FieldController principleCents;
+    @FXML private FieldController startDate;
+    @FXML private FieldController periodMonths;
+    @FXML private FieldController term;
+    @FXML private FieldController interestRate;
+    @FXML private FieldController rateType;
+    @FXML private FieldController compoundingFrequency;
+    @FXML private FieldController paymentAmountCents;
+    @FXML private FieldController paymentFrequency;
+    @FXML private FieldController interestOnly;
+    @FXML private FieldController loanPayments;
+    @FXML private FieldController loanSummary;
 
+    //loan summary details
+    @FXML private FieldController principle;
+    @FXML private FieldController rateValue;
+    @FXML private FieldController payoffDate;
+    @FXML private FieldController totalInterest;
+    @FXML private FieldController totalLoanCost;
+    @FXML private FieldController paymentAmount;
 
+    //loan payments details
+    @FXML private FieldController loanId;
+    @FXML private FieldController number;
+    @FXML private FieldController pages;
+    @FXML private FieldController payments;
+    @FXML private FieldController paymentInterests;
+    @FXML private FieldController paymentPrincipals;
+    @FXML private FieldController paymentRemainings;
+    @FXML private FieldController paymentNumbers;
+        
     public LoanBoxController() {
         ControllerUtils.loadFxml(this, "fragments/loan-box.fxml");
     }
