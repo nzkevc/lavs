aea54450c44b9661ac7c3961a11524b814763a4f
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-08-23 02:55:02 +1200

    feat: partially add searching for customers by name in search page

--- src/main/java/uoa/lavs/controllers/pages/SearchPageController.java
+++ src/main/java/uoa/lavs/controllers/pages/SearchPageController.java
@@ -1,24 +1,38 @@
 package uoa.lavs.controllers.pages;
 
+import java.util.List;
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uoa.lavs.App;
 import uoa.lavs.State;
+import uoa.lavs.models.Customer;
 import uoa.lavs.services.CustomerService;
 import uoa.lavs.utils.AsyncUtils;
 import uoa.lavs.utils.ControllerUtils;
 
 public class SearchPageController extends AnchorPane implements IPage {
   private static final Logger logger = LoggerFactory.getLogger(SearchPageController.class);
+  private static final int SEARCH_DELAY_SECONDS = 1;
+
+  private static enum SearchTypes {
+    ByName,
+    ByID
+  }
 
   @FXML private Button addButton;
   @FXML private Button goButton;
   @FXML private TextField customerIdInput;
+  @FXML private ListView<Customer> customerList;
+  @FXML private ChoiceBox<String> searchType;
   @FXML private Label notification;
 
   public SearchPageController() {
@@ -27,15 +41,25 @@ public class SearchPageController extends AnchorPane implements IPage {
 
   @FXML
   private void initialize() {
+    searchType.getItems().addAll(SearchTypes.values().toString());
+    searchType.setValue(SearchTypes.ByName.toString());
+
+    PauseTransition searchPause = new PauseTransition(Duration.seconds(SEARCH_DELAY_SECONDS));
+    searchPause.setOnFinished(e -> searchCustomer());
+
+    // only when the user stops typing, we search for the customer ^
     customerIdInput
         .textProperty()
         .addListener(
             (observable, oldValue, newValue) -> {
               notification.setVisible(false);
-              searchCustomer();
+              notification.setVisible(false);
+              searchPause.stop();
+              searchPause.play();
             });
-    customerIdInput.setOnAction(e -> goToCustomerPage());
-    goButton.setOnAction(e -> goToCustomerPage());
+    customerIdInput.setOnAction(e -> searchCustomer());
+    goButton.setOnAction(e -> searchCustomer());
+
     addButton.setOnAction(e -> addCustomer());
   }
 
@@ -47,14 +71,55 @@ public class SearchPageController extends AnchorPane implements IPage {
       return;
     }
 
+    if (searchType.getValue().equals(SearchTypes.ByName.toString())) {
+      searchCustomerByName(inputString);
+    } else if (searchType.getValue().equals(SearchTypes.ByID.toString())) {
+      searchCustomerById(inputString);
+    }
     AsyncUtils.promise(
         () -> CustomerService.getCustomer(inputString),
         (customer) -> {
-          displayFoundNotification(customer.getName());
+          displayFoundById(customer.getName());
           State.customerFromSearch.setValue(customer);
         },
         (Throwable e) -> {
           logger.debug("Error searching for customer: " + e.getMessage());
+          displayNotFoundNotification();
+          State.customerFromSearch.setValue(null);
+        });
+  }
+
+  private void searchCustomerByName(String inputString) {
+    logger.debug("Searching for customer with name: " + inputString);
+    AsyncUtils.promise(
+        () -> CustomerService.getCustomerListByName(inputString),
+        (customers) -> {
+          displayFoundCustomerNames(customers);
+        },
+        (Throwable e) -> {
+          logger.debug("Error searching for customer by name: " + e.getMessage());
+          displayNotFoundNotification();
+        });
+  }
+
+  private void displayFoundCustomerNames(List<Customer> customers) {
+    customerList.getItems().clear();
+    customerList.getItems().addAll(customers);
+    customerList.setVisible(true);
+  }
+
+  private void searchCustomerById(String inputString) {
+    logger.debug("Searching for customer with id: " + inputString);
+    AsyncUtils.promise(
+        () -> CustomerService.getCustomer(inputString),
+        (customer) -> {
+          customerList.getItems().clear();
+          customerList.getItems().add(customer);
+          displayFoundById(customer.getName());
+          State.customerFromSearch.setValue(customer);
+        },
+        (Throwable e) -> {
+          logger.debug("Error searching for customer by id: " + e.getMessage());
           displayNotFoundNotification();
           State.customerFromSearch.setValue(null);
         });
@@ -76,7 +141,7 @@ public class SearchPageController extends AnchorPane implements IPage {
     App.getMainController().switchPage(SummaryPageController.class);
   }
 
-  private void displayFoundNotification(String customerName) {
+  private void displayFoundById(String customerName) {
     notification.setText("Customer found: " + customerName);
     notification.setVisible(true);
   }
