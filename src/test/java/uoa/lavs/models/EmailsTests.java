e0c6ad62b8d495a93d428f364936cdb2133fe081
Gluglug7 <abod278@aucklanduni.ac.nz>
2024-08-23 14:03:05 +1200

    adding emails model tests

--- src/test/java/uoa/lavs/models/EmailsTests.java
+++ src/test/java/uoa/lavs/models/EmailsTests.java
@@ -1,3 +1,63 @@
 package uoa.lavs.models;
 
-public class EmailsTests {}
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class EmailsTests {
+  @Test
+  public void constructorTest() {
+    // Arrange
+    Emails emails = new Emails("123");
+    emails.setCustomerId("124");
+    Email primaryEmail = new Email(emails.getCustomerId(), 1, "123@123.com", true);
+
+    // Act
+    emails.setPrimaryEmail(primaryEmail);
+
+    // Assert
+    assertEquals(primaryEmail, emails.getPrimaryEmail());
+  }
+
+  @Test
+  public void constructor2Test() {
+    // Arrange
+    Email primaryEmail = new Email("123", 1, "123@123.com", true);
+    Emails emails = new Emails("123", primaryEmail);
+
+    // Act
+
+    // Assert
+    assertEquals(primaryEmail, emails.getPrimaryEmail());
+  }
+
+  @Test
+  public void changePrimaryEmailTest() {
+    // Arrange
+    Email primaryEmail = new Email("123", 1, "123@123.com", true);
+    Emails emails = new Emails("123", primaryEmail);
+
+    // Act
+    Email newPrimaryEmail = new Email("123", 2, "124@124.com", true);
+    emails.setPrimaryEmail(newPrimaryEmail);
+
+    // Assert
+    assertEquals(newPrimaryEmail, emails.getPrimaryEmail());
+    assertFalse(primaryEmail.getIsPrimary());
+  }
+
+  @Test
+  public void addEmailTest() {
+    // Arrange
+    Email primaryEmail = new Email("123", 1, "123@123.com", true);
+    Emails emails = new Emails("123", primaryEmail);
+
+    // Act
+    int firstEmailCount = emails.getEmailCount();
+    Email newEmail = new Email("123", 2, "124@124.com", false);
+    emails.addEmail(newEmail);
+
+    // Assert
+    assertEquals(firstEmailCount + 1, emails.getEmailCount());
+  }
+}
