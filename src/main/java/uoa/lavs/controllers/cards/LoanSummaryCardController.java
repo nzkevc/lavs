441f8287a32b6a1a4797e0828a9654820d3cba90
hayley9080 <hsmi309@aucklanduni.ac.nz>
2024-08-24 22:21:02 +1200

    feat: updated loansummary fields

--- src/main/java/uoa/lavs/controllers/cards/LoanSummaryCardController.java
+++ src/main/java/uoa/lavs/controllers/cards/LoanSummaryCardController.java
@@ -1,13 +1,76 @@
 package uoa.lavs.controllers.cards;
 
-import io.github.palexdev.materialfx.controls.MFXTableView;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import java.time.LocalDate;
+
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
+import uoa.lavs.mainframe.Frequency;
 import uoa.lavs.models.LoanSummary;
 
-public class LoanSummaryCardController {
-    
+public class LoanSummaryCardController extends ICard<LoanSummary> {
+
+    @FXML private MFXTextField loanIdField;
+    @FXML private MFXTextField customerIdField;
+    @FXML private MFXTextField customerNameField;
+    @FXML private MFXTextField principleField;
+    @FXML private MFXTextField rateValueField;
+    @FXML private MFXTextField payoffDateField;
+    @FXML private MFXTextField termField;
+    @FXML private MFXTextField totalInterestField;
+    @FXML private MFXTextField totalLoanCostField;
+    @FXML private MFXTextField paymentAmountField;
+    @FXML private MFXTextField paymentFrequencyField;
+
+    @Override
+    public void render(LoanSummary data) {
+
+        loanIdField.setText(data.getLoanId());
+        customerIdField.setText(data.getCustomerId());
+        customerNameField.setText(data.getCustomerName());
+        principleField.setText(data.getPriciple().toString());
+        rateValueField.setText(data.getRateValue().toString());
+        payoffDateField.setText(data.getPayoffDate().toString());
+        termField.setText(data.getTerm().toString());
+        totalInterestField.setText(data.getTotalInterest().toString());
+        totalLoanCostField.setText(data.getTotalLoanCost().toString());
+        paymentAmountField.setText(data.getPaymentAmount().toString());
+        paymentFrequencyField.setText(data.getPaymentFrequency().toString());
+    }
+
+    @Override
+    public void clear() {
+        loanIdField.clear();
+        customerIdField.clear();
+        customerNameField.clear();
+        principleField.clear();
+        rateValueField.clear();
+        payoffDateField.clear();
+        termField.clear();
+        totalInterestField.clear();
+        totalLoanCostField.clear();
+        paymentAmountField.clear();
+        paymentFrequencyField.clear();
+    }
+
+    @Override
+    public LoanSummary assemble() {
+        LoanSummary loanSummary = new LoanSummary();
+        loanSummary.setLoanId(loanIdField.getText());
+        loanSummary.setCustomerId(customerIdField.getText());
+        loanSummary.setCustomerName(customerNameField.getText());
+        loanSummary.setPriciple(Double.parseDouble(principleField.getText()));
+        loanSummary.setRateValue(Double.parseDouble(rateValueField.getText()));
+        loanSummary.setPayoffDate(LocalDate.parse(payoffDateField.getText()));
+        loanSummary.setTerm(Integer.parseInt(termField.getText()));
+        loanSummary.setTotalInterest(Double.parseDouble(totalInterestField.getText()));
+        loanSummary.setTotalLoanCost(Double.parseDouble(totalLoanCostField.getText()));
+        loanSummary.setPaymentAmount(Double.parseDouble(paymentAmountField.getText()));
+        loanSummary.setPaymentFrequency(Frequency.valueOf(paymentFrequencyField.getText()));
+        return loanSummary;
+    }
+   
+
+
 
    
 }
