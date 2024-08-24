e79803a215d461bd5cad99cc5e88cc89ab60f8d3
hayley9080 <hsmi309@aucklanduni.ac.nz>
2024-08-24 13:04:55 +1200

    feat: updated address card with textfields and added checkboxes for boolean

--- src/main/java/uoa/lavs/controllers/cards/AddressCardController.java
+++ src/main/java/uoa/lavs/controllers/cards/AddressCardController.java
@@ -3,7 +3,9 @@ package uoa.lavs.controllers.cards;
 import javafx.fxml.FXML;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import uoa.lavs.controllers.fragments.FieldController;
+
+import io.github.palexdev.materialfx.controls.MFXCheckbox;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import uoa.lavs.models.Address;
 import uoa.lavs.utils.ControllerUtils;
 
@@ -11,12 +13,14 @@ public class AddressCardController extends ICard<Address> {
 
   private static final Logger logger = LoggerFactory.getLogger(ContactCardController.class);
 
-  @FXML private FieldController addressLine1;
-  @FXML private FieldController addressLine2;
-  @FXML private FieldController suburb;
-  @FXML private FieldController city;
-  @FXML private FieldController postcode;
-  @FXML private FieldController country;
+  @FXML private MFXTextField addressLine1;
+  @FXML private MFXTextField addressLine2;
+  @FXML private MFXTextField suburb;
+  @FXML private MFXTextField city;
+  @FXML private MFXTextField postcode;
+  @FXML private MFXTextField country;
+  @FXML private MFXCheckbox isPrimary;
+  @FXML private MFXCheckbox isMailing;
 
   public AddressCardController() {
     ControllerUtils.loadFxml(this, "cards/address-card.fxml");
@@ -24,35 +28,39 @@ public class AddressCardController extends ICard<Address> {
 
   @Override
   public void render(Address address) {
-    addressLine1.setValue(address.getLine1());
-    addressLine2.setValue(address.getLine2());
-    suburb.setValue(address.getSuburb());
-    city.setValue(address.getCity());
-    postcode.setValue(address.getPostCode());
-    country.setValue(address.getCountry());
+    addressLine1.setText(address.getLine1());
+    addressLine2.setText(address.getLine2());
+    suburb.setText(address.getSuburb());
+    city.setText(address.getCity());
+    postcode.setText(address.getPostCode());
+    country.setText(address.getCountry());
+    isPrimary.setSelected(address.getPrimary());
+    isMailing.setSelected(address.getMailing());
   }
 
   @Override
   public void clear() {
-    addressLine1.clearValue();
-    addressLine2.clearValue();
-    suburb.clearValue();
-    city.clearValue();
-    postcode.clearValue();
-    country.clearValue();
+    addressLine1.clear();
+    addressLine2.clear();
+    suburb.clear();
+    city.clear();
+    postcode.clear();
+    country.clear();
+    isPrimary.setSelected(false);
+    isMailing.setSelected(false);
   }
 
   @Override
   public Address assemble() {
     Address address = new Address();
-    address.setLine1(addressLine1.getValue());
-    address.setLine2(addressLine2.getValue());
-    address.setSuburb(suburb.getValue());
-    address.setCity(city.getValue());
-    address.setPostCode(postcode.getValue());
-    address.setCountry(country.getValue());
-    address.setIsPrimary(true);
-    address.setIsMailing(true);
+    address.setLine1(addressLine1.getText());
+    address.setLine2(addressLine2.getText());
+    address.setSuburb(suburb.getText());
+    address.setCity(city.getText());
+    address.setPostCode(postcode.getText());
+    address.setCountry(country.getText());
+    address.setIsPrimary(isPrimary.isSelected());
+    address.setIsMailing(isMailing.isSelected());
     return address;
   }
 }
