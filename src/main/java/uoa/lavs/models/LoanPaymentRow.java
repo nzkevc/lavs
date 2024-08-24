53a7ecfffa6eb5f984924096605c871a315f7373
hayley9080 <hsmi309@aucklanduni.ac.nz>
2024-08-25 10:45:13 +1200

    feat: created model for a row in the LoanPayments table

---
+++ src/main/java/uoa/lavs/models/LoanPaymentRow.java
@@ -0,0 +1,32 @@
+package uoa.lavs.models;
+
+public class LoanPaymentRow {
+    private final double paymentInterest;
+    private final double paymentPrincipal;
+    private final double paymentRemaining;
+    private final int paymentNumber;
+
+    public LoanPaymentRow(double paymentInterest, double paymentPrincipal, double paymentRemaining, int paymentNumber) {
+        this.paymentInterest = paymentInterest;
+        this.paymentPrincipal = paymentPrincipal;
+        this.paymentRemaining = paymentRemaining;
+        this.paymentNumber = paymentNumber;
+    }
+
+    public double getPaymentInterest() {
+        return paymentInterest;
+    }
+
+    public double getPaymentPrincipal() {
+        return paymentPrincipal;
+    }
+
+    public double getPaymentRemaining() {
+        return paymentRemaining;
+    }
+
+    public int getPaymentNumber() {
+        return paymentNumber;
+    }
+}
+
