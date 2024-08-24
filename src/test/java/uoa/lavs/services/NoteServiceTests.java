ea8bf5ed37f3f855cb9e9c2a58f1890cd4c399a9
Gluglug7 <abod278@aucklanduni.ac.nz>
2024-08-24 20:47:16 +1200

    Adding note service tests

--- src/test/java/uoa/lavs/services/NoteServiceTests.java
+++ src/test/java/uoa/lavs/services/NoteServiceTests.java
@@ -1,3 +1,79 @@
 package uoa.lavs.services;
 
-public class NoteServiceTests {}
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uoa.lavs.TestEntityCreator;
+import uoa.lavs.models.Customer;
+import uoa.lavs.repository.CustomerRepository;
+import uoa.lavs.utils.objects.ConnectionInstance;
+
+public class NoteServiceTests {
+  @BeforeEach
+  public void resetTestConnection() {
+    ConnectionInstance.resetTestConnection();
+  }
+
+  @Test
+  public void updateNotesFromCustomerTest() {
+    // Arrange
+    NoteService noteService = new NoteService();
+    Customer customer = TestEntityCreator.createBasicCustomer();
+    customer = CustomerRepository.create(customer);
+
+    String notes = "This is a note";
+    customer.setNotes(notes);
+
+    // Act
+    NoteService.updateNotesFromCustomer(customer);
+    customer.setNotes(NoteService.getNotes(customer));
+
+    // Assert
+    assertEquals(notes, customer.getNotes());
+  }
+
+  @Test
+  public void updateMultipleNotesFromCustomer() {
+    // Arrange
+    Customer customer = TestEntityCreator.createBasicCustomer();
+    customer = CustomerRepository.create(customer);
+
+    String notes = "This is a note";
+    String notes2 = "This is another note";
+    customer.setNotes(notes);
+
+    // Act
+    NoteService.updateNotesFromCustomer(customer);
+    customer.setNotes(NoteService.getNotes(customer));
+
+    // Assert
+    assertEquals(notes, customer.getNotes());
+
+    // Act
+    customer.setNotes(notes2);
+    NoteService.updateNotesFromCustomer(customer);
+    customer.setNotes(notes);
+    customer.setNotes(NoteService.getNotes(customer));
+
+    // Assert
+    assertEquals(notes2, customer.getNotes());
+  }
+
+  @Test
+  public void updateNullNotesFromCustomerTest() {
+    // Arrange
+    Customer customer = TestEntityCreator.createBasicCustomer();
+    customer = CustomerRepository.create(customer);
+
+    String notes = null;
+
+    // Act
+    customer.setNotes(notes);
+    NoteService.updateNotesFromCustomer(customer);
+    customer.setNotes(NoteService.getNotes(customer));
+
+    // Assert
+    assertEquals("", customer.getNotes());
+  }
+}
