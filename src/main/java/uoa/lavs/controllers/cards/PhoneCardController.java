cddbc79d2ef7be1cc710b140daa2276cc8d5b08c
hayley9080 <hsmi309@aucklanduni.ac.nz>
2024-08-24 13:21:09 +1200

    feat: added fields and checkboxes for phoneCardController

--- src/main/java/uoa/lavs/controllers/cards/PhoneCardController.java
+++ src/main/java/uoa/lavs/controllers/cards/PhoneCardController.java
@@ -3,6 +3,9 @@ package uoa.lavs.controllers.cards;
 import javafx.fxml.FXML;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import io.github.palexdev.materialfx.controls.MFXCheckbox;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import uoa.lavs.controllers.fragments.FieldController;
 import uoa.lavs.models.Phone;
 import uoa.lavs.utils.ControllerUtils;
@@ -11,8 +14,11 @@ public class PhoneCardController extends ICard<Phone> {
 
   private static final Logger logger = LoggerFactory.getLogger(ContactCardController.class);
 
-  @FXML private FieldController prefix;
-  @FXML private FieldController phoneNumber;
+  @FXML private MFXTextField prefix;
+  @FXML private MFXTextField phoneNumber;
+  @FXML private MFXCheckbox isPrimary;
+  @FXML private MFXCheckbox isSendText;
+  
 
   public PhoneCardController() {
     ControllerUtils.loadFxml(this, "cards/phone-card.fxml");
@@ -20,23 +26,27 @@ public class PhoneCardController extends ICard<Phone> {
 
   @Override
   public void render(Phone phone) {
-    prefix.setValue(phone.getPrefix());
-    phoneNumber.setValue(phone.getPhoneNumber());
+    prefix.setText(phone.getPrefix());
+    phoneNumber.setText(phone.getPhoneNumber());
+    isPrimary.setSelected(phone.getPrimary());
+    isSendText.setSelected(phone.getCanSendTxt());
   }
 
   @Override
   public void clear() {
-    prefix.clearValue();
-    phoneNumber.clearValue();
+    prefix.clear();
+    phoneNumber.clear();
+    isPrimary.setSelected(false);
+    isSendText.setSelected(false);
   }
 
   @Override
   public Phone assemble() {
     Phone phone = new Phone();
-    phone.setPrefix(prefix.getValue());
-    phone.setPhoneNumber(phoneNumber.getValue());
-    phone.setPrimary(true);
-    phone.setCanSendTxt(true);
+    phone.setPrefix(prefix.getText());
+    phone.setPhoneNumber(phoneNumber.getText());
+    phone.setPrimary(isPrimary.isSelected());
+    phone.setCanSendTxt(isSendText.isSelected());
     return phone;
   }
 }
